888fcf056f9076d28c5181f066f69fbcaa647fc6
Christian Lopez <lopezchristian1610@gmail.com>
2023-07-09 17:24:18 -0400

    Fibonacci test passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibo = []
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        for i in range(length):
+            if(i == 0 or i == 1):
+                fibo.append(i)
+            else:
+                fibo.append(fibo[i - 1] + fibo[i - 2])
+        print(fibo)
